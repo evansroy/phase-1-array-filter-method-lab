6c97696ffca24ade824c8b870711a004d2574ee4
Evans <evansroysir@gmail.com>
2023-04-07 21:54:42 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+// Find all drivers that match the given name
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Find all drivers that begin with the given letters
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// Find all driver objects whose name property matches the given name
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
